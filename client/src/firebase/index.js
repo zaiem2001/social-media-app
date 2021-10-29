431bc27f3f134a469ccf913fd988e21bd0ea7e15
zaiem <zaiem.180410116045@gmail.com>
2021-10-29 23:48:11 +0530

    preparing for deployment

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyAWG8VLg67MqZ5gcjcWtRxNUkFH5C-CX2k",
   authDomain: "z-social-bf99d.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP,
+  projectId: "z-social-bf99d",
+  storageBucket: "z-social-bf99d.appspot.com",
+  messagingSenderId: "74331206869",
+  appId: "1:74331206869:web:138ea213a1b99e78bfde5c",
 };
 
 firebase.initializeApp(firebaseConfig);
